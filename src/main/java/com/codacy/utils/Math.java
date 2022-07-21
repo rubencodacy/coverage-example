75428d65294225b428277cf48dc86d22c3986eb7
rubensantoscodacy <ruben.santos@codacy.com>
2022-07-21 13:13:00 +0100

    Test

--- src/main/java/com/codacy/utils/Math.java
+++ src/main/java/com/codacy/utils/Math.java
@@ -15,7 +15,8 @@ public class Math {
         if (y == magicNumber) {
             return y - x;
         } else {
-	    System.out.println("hey");
+            System.out.println("hey");
+            System.out.println("hey");
             return x + y;
         }
     }
